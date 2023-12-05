9af95b0d7293a72cec4f0d8ddee9763c55826369
z924093568 <126219464+z924093568@users.noreply.github.com>
2023-12-05 20:37:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,49 +2,28 @@ import altair as alt
 import numpy as np
 import pandas as pd
 import streamlit as st
-
+import plotly.express as px
 """
-# Welcome to Streamlit!
+# Zhang Zixuan streamlit app :balloon:!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+The app is used to plot the time series within different CompanyID!
 """
-
-num_points = st.slider("Number of points in spiral", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
-
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
-
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
-
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
-
 
 pd_data = pd.read_pickle(filepath_or_buffer='./pd_data.pkl')
 
 option = st.selectbox(
-        'Select a companyID',
+        'Select a CompanyID',
         pd_data.index.levels[0])
 
-st.write(f'You selected companyID: {option}')
+st.write(f'You selected CompanyID: {option}')
 
+def line_charts(pd_data: pd.DataFrame, companyID: int):
+    pd_data_t = pd_data.copy()
+    sub = pd_data_t.loc[companyID]
+    sub.reset_index(inplace=True)
+    fig = px.line(sub, x="DataDate", y="PD", title=f'time series of Company {companyID}')
+    return fig
+
+fig = line_charts(pd_data, option)
+
+st.plotly_chart(fig, use_container_width=True)
